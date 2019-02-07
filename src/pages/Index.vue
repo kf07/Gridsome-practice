3b92ee932fab93b67df90598f34a287509043270
kango furusu <furusu0715@gmail.com>
2019-02-07 14:00:37 +0900

    sassテスト

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
     
-    <h1>Hello, world!</h1>
+    <h1 class="mainTitle">Hello, <span>Gridsome!</span></h1>
    
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
@@ -24,8 +24,14 @@ export default {
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
+<style lang="scss">
+  .home-links a {
+    margin-right: 1rem;
+  }
+  .mainTitle {
+    span {
+      font-size: 40px;
+      display: block;
+    }
+  }
 </style>
